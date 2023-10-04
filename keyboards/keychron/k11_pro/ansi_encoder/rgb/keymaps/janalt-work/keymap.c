1c19e538ac577b41cd01444aa46debc7ac781f98
Jace Altgen <jannik.altgen@gmail.com>
2023-10-04 07:36:29 +0200

    adding the work layout to have something to start working without being bogged down by funkier features

--- keyboards/keychron/k11_pro/ansi_encoder/rgb/keymaps/janalt-work/keymap.c
+++ keyboards/keychron/k11_pro/ansi_encoder/rgb/keymaps/janalt-work/keymap.c
@@ -22,7 +22,6 @@ enum layers{
     MAC_FN1,
     WIN_FN1,
     FN2,
-    // GAME,
 };
 
 
@@ -42,148 +41,22 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 };
 
 
-/// register custom keycodes to emulate umlaut behavior on Mac using the same mod dance technique as on windows
-enum custom_keycodes {
-    MAC_OUML_O,
-    MAC_AUML_A,
-    MAC_UUML_U,
-    MAC_SZLIG_S,
-};
-
-
-/// Layer Coloring https://docs.qmk.fm/#/feature_rgb_matrix?id=indicator-examples
-bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-
-   switch(get_highest_layer(layer_state|default_layer_state)) {
-            case 2:
-                rgb_matrix_set_color_all(RGB_PINK);
-                break;
-            case 1:
-                rgb_matrix_set_color_all(RGB_CYAN);
-                break;
-            default:
-                rgb_matrix_set_color_all(RGB_BLUE);
-                break;
-        }
-
-    for (uint8_t i = led_min; i < led_max; i++) {
-        if (HAS_FLAGS(g_led_config.flags[i], 0x01)) { // 0x01 == LED_FLAG_MODIFIER
-            rgb_matrix_set_color(i, RGB_ORANGE);
-        }
-    }
-    return false;
-}
-
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     for (uint8_t i = led_min; i < led_max; i++) {
-//         switch(get_highest_layer(layer_state|default_layer_state)) {
-//             case 2:
-//                 rgb_matrix_set_color(i, RGB_PINK);
-//                 break;
-//             case 1:
-//                 rgb_matrix_set_color(i, RGB_CYAN);
-//                 break;
-//             default:
-//                 rgb_matrix_mode(RGB_MATRIX_ALPHAS_MODS);
-//                 rgb_matrix_set_color(i, RGB_ORANGE);
-//                 break;
-//         }
-//     }
-//     return false;
-// }
-
-// crating cords to type Mac OS US. Intl. umlaut codes (LAlt+U - *letter*)
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case MAC_OUML_O:
-            if (record->event.pressed) {
-                // Simulate pressing and holding Alt+U
-                register_code(KC_LALT);
-                register_code(KC_U);
-
-                // Release Alt+U
-                unregister_code(KC_LALT);
-                unregister_code(KC_U);
-
-                // Send the desired letter (e.g., 'A')
-                register_code(KC_O);
-                unregister_code(KC_O);
-            }
-            break;
-        case MAC_AUML_A:
-            if (record->event.pressed) {
-                // Simulate pressing and holding Alt+U
-                register_code(KC_LALT);
-                register_code(KC_U);
-
-                // Release Alt+U
-                unregister_code(KC_LALT);
-                unregister_code(KC_U);
-
-                // Send the desired letter (e.g., 'A')
-                register_code(KC_A);
-                unregister_code(KC_A);
-            }
-            break;
-        case MAC_UUML_U:
-            if (record->event.pressed) {
-                // Simulate pressing and holding Alt+U
-                register_code(KC_LALT);
-                register_code(KC_U);
-
-                // Release Alt+U
-                unregister_code(KC_LALT);
-                unregister_code(KC_U);
-
-                // Send the desired letter (e.g., 'A')
-                register_code(KC_U);
-                unregister_code(KC_U);
-            }
-            break;
-        case MAC_SZLIG_S:
-            if (record->event.pressed) {
-                // Simulate pressing and holding Alt+S
-                register_code(KC_LALT);
-                register_code(KC_S);
-
-                // Release Alt+U
-                unregister_code(KC_LALT);
-                unregister_code(KC_S);
-            }
-            break;
-    }
-    return true;
-}
-
-
-
 // Tap Dance declarations
 enum {
-    TD_ALT_FN,
     TD_WIN_OUML_P,
     TD_WIN_AUML_Q,
     TD_WIN_UUML_Y,
     TD_WIN_SZLIG_S,
-    TD_MAC_OUML_O,
-    TD_MAC_AUML_A,
-    TD_MAC_UUML_U,
-    TD_MAC_SZLIG_S,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     // used for Umlaut in Win US Intl. keymap.
-    [TD_ALT_FN] = ACTION_TAP_DANCE_DOUBLE(OSL(4), KC_LALT),
     [TD_WIN_OUML_P] = ACTION_TAP_DANCE_DOUBLE(KC_O, RALT(KC_P)),
     [TD_WIN_AUML_Q] = ACTION_TAP_DANCE_DOUBLE(KC_A, RALT(KC_Q)),
     [TD_WIN_UUML_Y] = ACTION_TAP_DANCE_DOUBLE(KC_U, RALT(KC_Y)),
     [TD_WIN_SZLIG_S] = ACTION_TAP_DANCE_DOUBLE(KC_S, RALT(KC_S)),
-    [TD_MAC_OUML_O] = ACTION_TAP_DANCE_DOUBLE(KC_O, MAC_OUML_O),
-    [TD_MAC_AUML_A] = ACTION_TAP_DANCE_DOUBLE(KC_A, MAC_AUML_A),
-    [TD_MAC_UUML_U] = ACTION_TAP_DANCE_DOUBLE(KC_U, MAC_UUML_U),
-    [TD_MAC_SZLIG_S] = ACTION_TAP_DANCE_DOUBLE(KC_S, MAC_SZLIG_S),
 };
 
 
@@ -191,17 +64,17 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
-        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_G,    KC_J,     KC_L,    TD(TD_MAC_UUML_U),    KC_Y,    KC_SCLN,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_BSPC, TD(TD_MAC_AUML_A),     KC_R,     TD(TD_MAC_SZLIG_S),    KC_T,    KC_D,              KC_H,    KC_N,    KC_E,    KC_I,     TD(TD_MAC_OUML_O),  KC_QUOT,  KC_ENT,           KC_HOME,
+        KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_G,    KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
+        KC_BSPC, KC_A,     KC_R,     KC_S,    KC_T,    KC_D,              KC_H,    KC_N,    KC_E,    KC_I,     KC_O,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_K,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCMMD, KC_LCTL, KC_LOPTN,          LT(MAC_FN1,KC_SPC),           MO(MAC_FN1), MO(FN2),        LT(MAC_FN1,KC_SPC),            KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(MAC_FN1,KC_SPC),           MO(MAC_FN1), MO(FN2),        LT(MAC_FN1,KC_SPC),            KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,     KC_W,     KC_F,    KC_P,    KC_G,    KC_J,     KC_L,    TD(TD_WIN_UUML_Y),    KC_Y,    KC_SCLN,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_BSPC, TD(TD_WIN_AUML_Q),     KC_R,     TD(TD_WIN_SZLIG_S),    KC_T,    KC_D,              KC_H,    KC_N,    KC_E,    KC_I,     TD(TD_WIN_OUML_P),  KC_QUOT,  KC_ENT,           KC_HOME,
+        KC_BSPC, TD(TD_WIN_AUML_Q),  KC_R,     TD(TD_WIN_SZLIG_S),    KC_T,    KC_D,              KC_H,    KC_N,    KC_E,    KC_I,     TD(TD_WIN_OUML_P),  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_K,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN,  TD(TD_ALT_FN),           LT(MAC_FN1,KC_SPC),           MO(MAC_FN1), MO(FN2),       LT(MAC_FN1,KC_SPC),            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN,  KC_LALT,  LT(WIN_FN1,KC_SPC),           MO(WIN_FN1), MO(FN2),       LT(WIN_FN1,KC_SPC),            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_FN1] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
